18cadc3d258b098f700740b8036a988d621c295d
Pavel <pasha.krupko@yandex.ru>
2021-02-25 14:39:05 +0300

    Вносит правки по замечаниям тестирования

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -8,16 +8,34 @@
 
   for (var i = 0; i < accordions.length; i++) {
     accordions[i].classList.remove('accordion--open');
-    accordionLists[i].classList.add('accordion-list--hide');
+    accordionLists[i].classList.add('accordion-list--close');
   }
 
   btnAccordionContainer.addEventListener('click', function (evt) {
 
     if (evt.target.classList.contains('accordion')) {
+      var accordionsOpen = document.querySelectorAll('.accordion--open');
 
       var accordionList = document.querySelector('.' + evt.target.classList.value.slice(0, nameLength) + ' + ul');
       evt.target.classList.toggle('accordion--open');
-      accordionList.classList.toggle('accordion-list--hide');
+      accordionList.classList.toggle('accordion-list--close');
+
+      for (var j = 0; j < accordionsOpen.length; j++) {
+        if (accordionsOpen[j].classList.value !== 'accordion--02 accordion' && accordionsOpen[j].classList.value !== 'accordion--01 accordion') {
+          accordionsOpen[j].classList.toggle('accordion--open');
+        }
+
+        var accordionLists01 = btnAccordionContainer.querySelector('.accordion-list--01');
+        var accordionLists02 = btnAccordionContainer.querySelector('.accordion-list--02');
+
+        if (evt.target.classList.contains('accordion--01')) {
+          accordionLists02.classList.add('accordion-list--close');
+        }
+
+        if (evt.target.classList.contains('accordion--02')) {
+          accordionLists01.classList.add('accordion-list--close');
+        }
+      }
     }
   });
 })();
